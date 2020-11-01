6b9d8aec4262fbb9ce48d7b1eb390c70d3945300
SaquibAnsari0101 <asaquib454@gmail.com>
2020-11-01 18:34:43 +0530

    Update the code

--- app/src/main/java/com/sqb/stringpassing/SecondActivity.java
+++ app/src/main/java/com/sqb/stringpassing/SecondActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.sqb.android.stringpassing.extra.REPLY";
+    public static final String LOG_TAG =SecondActivity.class.getSimpleName();
     private EditText mReply;
 
     @Override
@@ -28,6 +30,7 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG,"END SECONDACTIVITY");
         finish();
     }
 }
